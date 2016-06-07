dd34d983bdea9ff70eabe7267292dd1b3acd7882
Dominik Burgdörfer <db@domachine.de>
2016-06-07 13:49:18 +0200

    Wire up mockgoose for tests
    This closes #2.

--- spec/sample.js
+++ spec/sample.js
@@ -3,11 +3,17 @@ import test from 'ava'
 import request from 'request-promise'
 import listen from 'test-listen'
 import mongoose from 'mongoose'
-
-mongoose.connect('mongodb://localhost/test')
+import mockgoose from 'mockgoose'
+import Bluebird from 'bluebird'
 
 import app from '..'
 import sample from './sample/index.js'
+
+test.before(async () => {
+  await mockgoose(mongoose)
+  const connect = Bluebird.promisify(mongoose.connect, { context: mongoose })
+  await connect('mongodb://localhost:27019/test')
+})
 
 test('the sample is accepted with 200', async t => {
   t.plan(1)

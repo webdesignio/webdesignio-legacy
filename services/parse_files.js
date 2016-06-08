52872053eed7d0f20059e63ff1e5676feae46902
Dominik Burgdörfer <db@domachine.de>
2016-06-09 01:02:20 +0200

    Fix racing upload streams

--- services/parse_files.js
+++ services/parse_files.js
@@ -9,7 +9,15 @@ module.exports = parseFiles
 function parseFiles (req) {
   const busboy = new Busboy({ headers: req.headers })
   const validTypes = ['pages', 'objects', 'components', 'client']
+  let count = 0
+  let finished = false
   return Observable.create(observer => {
+    const finish = () => {
+      if (count === 0 && !finished) {
+        finished = true
+        observer.onCompleted()
+      }
+    }
     busboy.on('file', (fieldname, file, filename) => {
       const type = fieldname.split('/')[0]
       if (validTypes.indexOf(type) === -1) {
@@ -17,18 +25,19 @@ function parseFiles (req) {
         file.resume()
         return
       }
+      ++count
       const stream = temp.createWriteStream()
       file
-        .on('end', () => {
-          observer.onNext({ type, filename: fieldname, path: stream.path })
-        })
         .pipe(stream)
         .on('error', err => observer.onError(err))
+        .on('close', () => {
+          --count
+          observer.onNext({ type, filename: fieldname, path: stream.path })
+          finish()
+        })
     })
     busboy.on('error', err => observer.onError(err))
-    busboy.on('finish', () => {
-      observer.onCompleted()
-    })
+    busboy.on('finish', finish)
     req.pipe(busboy)
   })
   .publish()

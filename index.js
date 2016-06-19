7e657efda4b8c97212d964332823b386e0f0e971
Dominik Burgdörfer <db@domachine.de>
2016-06-19 10:18:46 +0200

    Move app initialization to server.js

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const express = require('express')
-
-const app = module.exports = express()
-
-app.use('/api/v1', require('./services/api/v1'))
-app.use(require('./services/editing'))
--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const { log } = require('util')
 const http = require('http')
 const mongoose = require('mongoose')
 const config = require('config')
+const express = require('express')
 
 mongoose.connect(config.get('mongoUrl'))
 
-const app = require('.')
+const app = express()
+
+app.use('/api/v1', require('./services/api/v1'))
+app.use(require('./services/editing'))
 
 const server = http.createServer(app)
 server.listen(process.env.PORT || 3000, () => {
